f39596fde8e3fea5441b4f4a79bb605f9c800ea7
reiser <pingchi.wang@gmail.com>
2023-03-09 20:38:19 +0800

    Update prime_number_finder_pool_map.py

--- prime_number_finder_pool_map.py
+++ prime_number_finder_pool_map.py
@@ -1,34 +1,37 @@
-import math,time
+import math
+import time
 from multiprocessing import Pool
 
-def isPrime(x):
-    if x <2:
-        return 0
-    if x==2:
-        return x
+#The algorithm used here is called the "Sieve of Eratosthenes," which iteratively identifies prime numbers by eliminating multiples of known primes. 
+def is_prime(x):
+    if x < 2:
+        return False
+    elif x == 2 or x == 3:
+        return True
+    elif x % 2 == 0 or x % 3 == 0:
+        return False
     else:
-        for div in range(2,int(math.sqrt(x) +1)):
-            if ((x % div) ==0):
-                return 0
-    return x
+        for i in range(5, int(math.sqrt(x)) + 1, 6):
+            if x % i == 0 or x % (i + 2) == 0:
+                return False
+        return True
 
 
-if __name__ == '__main__':
-    cleaned=[]
-    var = input("Find prime numbers up to: ")
-    proc=int(input("# of parallel worker processes:"))
-    """
-    time.clock has been deprecated in Python 3.3 and will be removed from Python 3.8: use time.perf_counter or time.process_time instead
-    """
-   
-    #start = time.clock()
-    pool= Pool(processes=proc)
-    result =  pool.map(isPrime, range(2,int(var)))
+if __name__ == "__main__":
+    cleaned = [2,3]
+    var = int(input("Find prime numbers up to: "))
+    proc = int(input("# of parallel worker processes: "))
+    
+    start = time.perf_counter()
+    #we use a with statement to create the Pool object, which ensures that the pool is cleaned up properly after use.
+    with Pool(processes=proc) as pool:
+        result = pool.map(is_prime, range(5, var,6))
+
     for x in result:
-        if x!=0:
+        if x:
             cleaned.append(x)
 
-    #elapsed = (time.clock()-start)
+    elapsed = time.perf_counter() - start
     print(cleaned)
-    print ('total prime numbers:',len(cleaned)) 
-    #print ('run time:',elapsed)
+    print("total prime numbers:", len(cleaned))
+    print("run time:", elapsed)
